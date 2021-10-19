6b0e9e866b894aa200d9a01a3ac4d545864a8fd3
Peyton Cornelison <peyton.corn8@gmail.com>
2021-10-18 20:21:57 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const connectionString = 'mongodb+srv://peyton123:peyton1234@cluster0.e5iad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+MongoClient.connect(connectionString)
+.then(client => {
+  const db = client.db('expense-tracker');
+  const expensesCollection = db.collection('expenses');
+
+  app.post('/expenses', (req, res) => {
+    expensesCollection.insertOne(req.body)
+      .then(() => {
+        res.send(req.body);
+      })
+      .catch(error => console.error(error))
+  });
+ 
+  app.get('/expenses', (req, res) => {
+  
+  });
+})
+.catch(error => console.error(error))
+
+app.listen(3001, () => {
+    console.log('running');
+});
